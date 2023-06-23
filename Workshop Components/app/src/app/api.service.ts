0d2358f38f55d76ce77d185e44da23d967c96f7d
Daniel Kamenov <sorostmcoc@gmail.com>
2023-06-23 22:28:46 +0300

    add header and footer components

---
+++ Workshop Components/app/src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  loadThemes() {
+    return this.httpClient.get('http://localhost:3000/api/themes')
+  }
+
+  loadPosts() {
+    return this.httpClient.get('https://localhost:3000/api/posts?limit=5')
+  }
+}
---
+++ Workshop Components/app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThemeListComponent,
+    RecentPostsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
