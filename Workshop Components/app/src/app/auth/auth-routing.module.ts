88316f1b6be60addbf8db8c0a067de79098ad679
Daniel Kamenov <sorostmcoc@gmail.com>
2023-06-25 13:33:34 +0300

    added auth guard

--- Workshop Components/app/src/app/auth/auth-routing.module.ts
+++ Workshop Components/app/src/app/auth/auth-routing.module.ts
@@ -3,12 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthActivate } from '../shared/guards/auth.activate';
 
 const routes: Routes = [
-    {path: 'auth/login', component: LoginComponent, data: {title: 'Login'}},
-    {path: 'auth/register', component: RegisterComponent, data: {title: 'Register'}},
-    {path: 'auth/logout', component: LogoutComponent, data: {title: 'Logout'}},
-    {path: 'auth/profile', component: ProfileComponent, data: {title: 'Profile'}}
+    {path: 'auth/login', component: LoginComponent, canActivate: [AuthActivate],data: {title: 'Login', loginRequired: false}},
+    {path: 'auth/register', component: RegisterComponent, canActivate: [AuthActivate], data: {title: 'Register', loginRequired: false}},
+    {path: 'auth/logout', component: LogoutComponent, canActivate: [AuthActivate], data: {title: 'Logout', loginRequired: true}},
+    {path: 'auth/profile', component: ProfileComponent, canActivate: [AuthActivate], data: {title: 'Profile', loginRequired: true}}
 ];
 
 
