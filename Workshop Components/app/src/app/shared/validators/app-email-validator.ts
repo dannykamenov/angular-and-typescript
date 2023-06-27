81bdca008b5221f1585c051ee51967f2681f39ee
Daniel Kamenov <sorostmcoc@gmail.com>
2023-06-27 20:22:12 +0300

    fixed regex

--- Workshop Components/app/src/app/shared/validators/app-email-validator.ts
+++ Workshop Components/app/src/app/shared/validators/app-email-validator.ts
@@ -1,9 +1,10 @@
 import { ValidatorFn } from "@angular/forms";
 
-export const appEmailValidator: ValidatorFn = (control) => {
-    const emailRegex = new RegExp('[^@]{6,}@gmail\.(com|bg)');
-    if (control.value && !emailRegex.test(control.value)) {
-        return { invalidEmail: true };
+export function appEmailValidator(domains: string[]): ValidatorFn {
+    const domainString = domains.join('|');
+    const re = new RegExp(`^[^@]{6,}@gmail\.(${domainString})$`);
+    return (control) => {
+        // /[^@]{6,}@gmail\.(com|bg)$/
+        return .test(control.value) ? null : { eppEmail: true };
     }
-    return null;
 }
