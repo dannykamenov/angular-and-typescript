6fdcea485dfb8366d78281b40cda8a38b31c10d7
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-01 00:55:38 +0300

    piping

--- Workshop Components/app/src/app/core/error/error.component.ts
+++ Workshop Components/app/src/app/core/error/error.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import { BehaviorSubject, take } from 'rxjs';
 import { API_ERROR } from 'src/app/shared/constants';
 
 @Component({
@@ -11,6 +12,10 @@ export class ErrorComponent {
 
   apiError$ = this.apiError.asObservable();
 
-  constructor(@Inject(API_ERROR) private apiError: BehaviorSubject<Error | null>, ) { }
+  constructor(@Inject(API_ERROR) private apiError: BehaviorSubject<Error | null>, private router: Router) { 
+    this.apiError$.pipe(take(1)).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
 
 }
