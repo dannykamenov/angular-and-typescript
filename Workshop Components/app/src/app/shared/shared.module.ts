8d4f9ab9f3eb1d457f82f4be7326c9925b7b57cd
Daniel Kamenov <sorostmcoc@gmail.com>
2023-07-01 19:13:56 +0300

    added pipes

--- Workshop Components/app/src/app/shared/shared.module.ts
+++ Workshop Components/app/src/app/shared/shared.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { AppEmailDirective } from './validators/app-email.directive';
+import { ShortenPipe } from './pipes/shorten.pipe';
+import { ElapsedTimePipe } from './pipes/elapsed-time.pipe';
 
 
 
 @NgModule({
   declarations: [
     LoaderComponent,
-    AppEmailDirective
+    AppEmailDirective,
+    ShortenPipe,
+    ElapsedTimePipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LoaderComponent,
-    AppEmailDirective
+    AppEmailDirective,
+    ShortenPipe
   ]
 })
 export class SharedModule { }
